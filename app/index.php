abff366cf5573b870e5b2d85e143960690b4d2e6
Franco Lippi <soyflippi@gmail.com>
2021-03-25 13:03:25 -0300

    update slim 4

--- app/index.php
+++ app/index.php
@@ -1,20 +1,24 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+error_reporting(-1);
+ini_set('display_errors', 1);
 
-require_once '../vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
 
+require __DIR__ . '/../vendor/autoload.php';
 
+// Instantiate App
+$app = AppFactory::create();
 
-$app = new \Slim\App([]);
-
-
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
+    $response->getBody()->write("GET => Bienvenido!!! a SlimFramework");
     return $response;
 
 });
-
 
 $app->run();
